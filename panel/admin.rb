f384fa2c52c5f636c1767f5202b90cc3ffdafed1
elgun22 <elgunhaydarov22@icloud.com>
2024-12-01 00:00:47 +0300

    k'

--- panel/admin.rb
+++ panel/admin.rb
@@ -1,14 +1,17 @@
 require 'socket'
 require 'google/protobuf'
 require_relative 'dist_servers_pb'
+#require_relative 'Capacity_pb'
 
 class AdminPanel
   SERVER_PORTS = [5001, 5002, 5003]
   SERVERS = []
+  PLOTTER_SOCKET = nil
 
   def initialize
     read_config
     connect_to_servers
+    #connect_to_plotter
     start_servers
     handle_capacity_requests
   end
@@ -33,6 +36,26 @@ class AdminPanel
     end
   end
 
+  #def connect_to_plotter
+  #  begin
+  #    @plotter_socket = TCPSocket.new('localhost', 6000)
+  #    puts "Plotter sunucusuna bağlanıldı"
+  #  rescue => e
+  #    puts "Plotter sunucusuna bağlantı başarısız: #{e.message}"
+  #    @plotter_socket = nil
+  #  end
+  #end
+
+  def send_message_to_all_servers(message)
+    socket.puts("ADMIN")
+
+    SERVERS.each do |socket|
+      socket.write(message.to_proto)
+      response = DistServers::Message.decode(socket.read)
+      handle_response(response)
+    end
+  end
+
   def start_servers
     config = DistServers::Configuration.new(
       fault_tolerance_level: @config['fault_tolerance_level'].to_i,
@@ -41,40 +64,20 @@ class AdminPanel
     send_message_to_all_servers(config)
   end
 
-  def handle_capacity_requests
-    loop do
-      sleep 5
-      send_capacity_request
-    end
-  end
+  #def send_capacity_request
+  #  message = DistServers::Message.new(demand: :CPCTY)
+  #  SERVERS.each do |socket|
+  #    socket.write(message.to_proto)
+  #    response = socket.read
+  #    handle_capacity_response(response)
+  #  end
+  #end
 
-  def send_capacity_request
-    message = DistServers::Message.new(demand: :CPCTY)
-    send_message_to_all_servers(message)
-  end
-
-  def send_message_to_all_servers(message)
-    SERVERS.each do |socket|
-      socket.write(message.to_proto)
-      response = DistServers::Message.decode(socket.read)
-      handle_response(response)
-    end
-  end
-
-  def handle_response(response)
-    case response.response
-    when :YEP
-      puts "Yanıt: Evet"
-    when :NOP
-      puts "Yanıt: Hayır"
-    else
-      puts "Bilinmeyen yanıt"
-    end
-  end
-
-  def close_connections
-    SERVERS.each(&:close)
-  end
+  #def handle_capacity_response(response)
+  #  return unless @plotter_socket
+  #  data = DistServers::Capacity.decode(response)
+  #  @plotter_socket.write(data.to_proto)
+  #end
 end
 
 admin_panel = AdminPanel.new
