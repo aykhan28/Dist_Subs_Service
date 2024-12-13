890eea0aa9319cd8b04771f8ae66a46065f71076
Aykhan Shirinzade <aykhanshirinzade28@gmail.com>
2024-12-13 03:37:31 +0300

    basit güncelleme

--- dist_servers/Server1.java
+++ dist_servers/Server1.java
@@ -19,7 +19,7 @@ public class Server1 {
             System.out.println("Server1 çalışıyor ve bağlantıları bekliyor...");
 
             Socket adminClient = adminSoket.accept();
-            new Thread(new AdminHandler(adminClient, kilit)).start();
+            new Thread(new AdminHandler(adminClient)).start();
 
             new Thread(new DistributedServerHandler(5002, "Server2", server1Socket, server2Socket, server3Socket)).start();
             new Thread(new DistributedServerHandler(5003, "Server3", server1Socket, server2Socket, server3Socket)).start();
--- dist_servers/Server2.java
+++ dist_servers/Server2.java
@@ -19,7 +19,7 @@ public class Server2 {
             System.out.println("Server2 çalışıyor ve bağlantıları bekliyor...");
 
             Socket adminClient = adminSoket.accept();
-            new Thread(new AdminHandler(adminClient, kilit)).start();
+            new Thread(new AdminHandler(adminClient)).start();
 
             new Thread(new DistributedServerHandler(5001, "Server1", server1Socket, server2Socket, server3Socket)).start();
             new Thread(new DistributedServerHandler(5003, "Server3", server1Socket, server2Socket, server3Socket)).start();
